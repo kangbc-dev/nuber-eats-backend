394d73356951263b4f6527c1b40515a634462fe6
kangbc-dev <kangbc.dev@gmail.com>
2024-10-21 17:54:48 +0900

    autoSchemaFile: true로 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,13 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
-import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
-      autoSchemaFile: join(
-        process.cwd(),
-        'src/schema-gql',
-      ) /* process.cwd() 현재 작업 절대경로 ,path.join('/Users/kangbc/dev/nomadcoder/nuber-eats-backend/nuber-eats-backend', 'src/schema-gql') === '/src/schema.gql' 경로 합쳐주는 메소드.  */,
+      autoSchemaFile: true, // 메모리로부터 schema.gql 파일을 생성, 읽음
       driver: ApolloDriver,
     }),
-    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
