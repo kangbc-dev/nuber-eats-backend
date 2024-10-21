2770e60ac7977c7370d4dee8b5fabc830996f645
kangbc-dev <kangbc.dev@gmail.com>
2024-10-21 18:34:15 +0900

    add typeOrmModule, ConfigModule in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,28 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true, // 메모리로부터 schema.gql 파일을 생성, 읽음
       driver: ApolloDriver,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'kangbc',
+      password: '',
+      database: 'nuber-eats-clone',
+      synchronize: true,
+      logging: true,
     }),
     RestaurantsModule,
   ],
