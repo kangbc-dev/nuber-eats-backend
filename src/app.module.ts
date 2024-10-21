9e73b6d0c89cf6d248ea371d80cb3bd81251065d
kangbc-dev <kangbc.dev@gmail.com>
2024-10-21 17:34:51 +0900

    path.join(), process.cwd() 주석 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,10 @@ import { join } from 'path';
 @Module({
   imports: [
     GraphQLModule.forRoot({
-      autoSchemaFile: join(process.cwd(), 'src/schema-gql'),
+      autoSchemaFile: join(
+        process.cwd(),
+        'src/schema-gql',
+      ) /* process.cwd() 현재 작업 절대경로 ,path.join('/Users/kangbc/dev/nomadcoder/nuber-eats-backend/nuber-eats-backend', 'src/schema-gql') === '/src/schema.gql' 경로 합쳐주는 메소드.  */,
       driver: ApolloDriver,
     }),
   ],
