3d9a0b8fc2857f38eb6e4f4e3c0aa4b693afe8e4
kangbc-dev <kangbc.dev@gmail.com>
2024-10-21 17:54:35 +0900

    add restaurants r

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { RestaurantsResolver } from './restaurants.resolver';
+import { Query } from '@nestjs/graphql';
 
-@Module({})
+@Module({
+  providers: [RestaurantsResolver],
+})
 export class RestaurantsModule {}
